4ef134de1edd387013a13d162f96c761fd36a5f5
Ordem <ordemdigitale@gmail.com>
2025-06-29 11:32:45 +0000

    changed port to 3004

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+const app = next({ dev: false })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(3004, () => console.log('> Ready on http://localhost:3004'))
+})
