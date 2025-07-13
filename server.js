749eb2b5a3eac7826c670d65cef34cb7497a564d
Ordem <ordemdigitale@gmail.com>
2025-07-13 11:04:54 +0000

    updated server.js file

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 // server.js
-import { createServer } from 'http';
-import { parse } from 'url';
-import next from 'next';
-const app = next({ dev: false })
+
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const port = parseInt(process.env.PORT || '3004', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(3004, () => console.log('> Ready on http://localhost:3004'))
+  }).listen(port, (err) => {
+    if (err) throw err;
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`);
+  });
 })
